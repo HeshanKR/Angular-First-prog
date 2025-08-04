1783e8a1f9da8a1f293a11da9719747cf94f5030
Bashith <bashith3214@gmail.com>
2025-08-04 20:27:35 +0530

    Refactor dashboard component: add CommonModule import and conditionally display Admin Role Editor button for admin users

--- angFrontend/src/app/pages/dashboard/dashboard.component.ts
+++ angFrontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(public authService: AuthService, private router: Router) {}
 
   logout() {
     this.authService.logout();
