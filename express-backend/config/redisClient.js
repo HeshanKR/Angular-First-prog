330010fe0f6adcd3709ddfc452b4233a86585a07
Bashith <bashith3214@gmail.com>
2025-08-10 21:14:21 +0530

    Update project documentation: add folder and file structure for Angular frontend and Express backend

--- express-backend/config/redisClient.js
+++ express-backend/config/redisClient.js
@@ -16,21 +16,3 @@ async function connectRedis() {
 }
 
 module.exports = { redisClient, connectRedis };
-// require("dotenv").config();
-// const { createClient } = require("redis");
-
-// const redisClient = createClient({
-//   url: process.env.UPSTASH_REDIS_URL,
-//   password: process.env.UPSTASH_REDIS_TOKEN,
-// });
-
-// redisClient.on("error", (err) => console.error("Redis Client Error", err));
-
-// async function connectRedis() {
-//   if (!redisClient.isOpen) {
-//     await redisClient.connect();
-//     console.log("Connected to Upstash Redis!");
-//   }
-// }
-
-// module.exports = { redisClient, connectRedis };
--- express-backend/routes/user.routes.js
+++ express-backend/routes/user.routes.js
@@ -33,40 +33,6 @@ async function checkUserCache(req, res, next) {
   }
 }
 
-// router.get("/me", generalLimiter, authenticateToken, (req, res) => {
-//   // req.user comes from the token payload
-//   res.json({
-//     id: req.user.id,
-//     name: req.user.name,
-//     email: req.user.email,
-//     role: req.user.role,
-//   });
-// });
-
-// // PATCH /:id/role - Admin only
-// router.patch(
-//   "/:id/role",
-//   strictLimiter,
-//   authenticateToken,
-//   authorizeRoles("admin"),
-//   [
-//     body("role")
-//       .exists()
-//       .withMessage("Role is required")
-//       .isIn(["admin", "user"])
-//       .withMessage("Role must be either 'admin' or 'user'"),
-//   ],
-//   (req, res, next) => {
-//     // Run the validations and check for errors
-//     const errors = require("express-validator").validationResult(req);
-//     if (!errors.isEmpty()) {
-//       return res.status(400).json({ errors: errors.array() });
-//     }
-//     next();
-//   },
-//   updateUserRole
-// );
-
 router.get(
   "/me",
   generalLimiter,
